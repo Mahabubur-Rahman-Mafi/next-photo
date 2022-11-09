3ea47d41b51e3b3ed3eeda7e0879e45b6045fa28
Mahabubur Rahman <mrmafi32@gmail.com>
2022-11-09 18:13:13 +0600

    services done

---
+++ src/Pages/Home/SerCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from "react-bootstrap/Card";
+
+const SerCard = ({ s }) => {
+    const { name, des, image} = s;
+    console.log(s);
+    return (
+      <Card>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>
+           {des.split('',100)}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+};
+
+export default SerCard;
--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import SerCard from './SerCard';
+import './Home.css'
+import { Link } from 'react-router-dom';
 
 const Services = () => {
+  const [ser, setSer] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:5000/services3")
+      .then(res => res.json())
+      .then(data => setSer(data))
+    .catch(e=>console.log(e))
+  }, [])
     return (
-      <div>
-        <h1 className="text-center mb-4">Services</h1>
-      </div>
+      <>
+        <Container>
+          <h1 className="text-center mt-5 ">Services</h1>
+          <div className="service-section">
+            {ser.map((s) => (
+              <SerCard s={s} key={s._id}></SerCard>
+            ))}
+          </div>
+          <Link className='text-center'>
+            <Button className="service-button text-end">
+              View Some
+            </Button>
+          </Link>
+        </Container>
+      </>
     );
 };
 
