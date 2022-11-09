907f1424df2ba01c1026ac5b3517372cb3311e00
Mahabubur Rahman <mrmafi32@gmail.com>
2022-11-09 19:30:14 +0600

    single service done

--- src/Pages/Services/ServiceDetails.jsx
+++ src/Pages/Services/ServiceDetails.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { useLoaderData } from "react-router-dom";
 import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const ServiceDetails = () => {
-    const service = useLoaderData();
-    const { image, name, des, price } = service;
-    console.log(service);
-    return (
-      <Container>
+  const service = useLoaderData();
+  const { image, name, des, price } = service;
+  console.log(service);
+  return (
+    <Container>
+      <h1 className="mt-4">{name}</h1>
+      <PhotoProvider>
         <PhotoView src={image}>
-          <img src={image} style={{ objectFit: "cover" }} alt="" />
+          <img src={image} alt="" className="w-100 mt-4" />
         </PhotoView>
-      </Container>
-    );
+        {/* details section  */}
+        <h2 className="mt-4">Details</h2>
+        <p>{des}</p>
+        {/* ___ */}
+      </PhotoProvider>
+    </Container>
+  );
 };
 
 export default ServiceDetails;
