0418e8deff9df2be28c9a27de0e9568a649521d7
Mahabubur Rahman <mrmafi32@gmail.com>
2022-11-10 18:11:09 +0600

    blog update

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
-
+import Card from "react-bootstrap/Card";
+import image from '../../Assets/blog/jwt-min.jpg'
+import image2 from '../../Assets/blog/node-min.jpg'
+import image3 from '../../Assets/blog/nodejs-min.jpg'
+import image4 from '../../Assets/blog/sql-min.jpg'
 const Blog = () => {
     return (
       <>
@@ -9,7 +13,60 @@ const Blog = () => {
           <title>Blog | NEXT </title>
         </Helmet>
         <Container>
-          <h1>This is blog section</h1>
+          <Card className="mb-4">
+            <Card.Img variant="top" src={image4} />
+            <Card.Body>
+              <Card.Title>Difference between SQL and NoSQL</Card.Title>
+              <Card.Text>
+                SQL is the programming language used to interface with
+                relational databases. (Relational databases model data as
+                records in rows and tables with logical links between them).
+                NoSQL is a class of DBMs that are non-relational and generally
+                do not use SQL.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="mb-4">
+            <Card.Img variant="top" src={image3} />
+            <Card.Body>
+              <Card.Title>
+                What is the difference between javascript and NodeJS?
+              </Card.Title>
+              <Card.Text>
+                JavaScript is a simple programming language that can be used
+                with any browser that has the JavaScript Engine installed. Node.
+                js, on the other hand, is an interpreter or execution
+                environment for the JavaScript programming language.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="mb-4">
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+              <Card.Title>What is JWT, and how does it work?</Card.Title>
+              <Card.Text>
+                JSON Web Token (JWT) is an open standard (RFC 7519) that defines
+                a compact and self-contained way for securely transmitting
+                information between parties as a JSON object. This information
+                can be verified and trusted because it is digitally signed.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="mb-4">
+            <Card.Img variant="top" src={image2} />
+            <Card.Body>
+              <Card.Title>
+                How does NodeJS handle multiple requests at the same time?
+              </Card.Title>
+              <Card.Text>
+                How NodeJS handle multiple client requests? NodeJS receives
+                multiple client requests and places them into EventQueue. NodeJS
+                is built with the concept of event-driven architecture. NodeJS
+                has its own EventLoop which is an infinite loop that receives
+                requests and processes them.
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Container>
       </>
     );
--- src/Pages/Personal/RevCard.jsx
+++ src/Pages/Personal/RevCard.jsx
@@ -1,103 +1,114 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import toast from 'react-hot-toast';
-import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
-import { UserAuth } from '../../Auth/AuthContext';
+import toast from "react-hot-toast";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import { UserAuth } from "../../Auth/AuthContext";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 
-
 const RevCard = ({ rev }) => {
-    const { user } = useContext(UserAuth)
-  const { serviceName, text, _id } = rev
-  const [rest, setRest] = useState()
+  const { user } = useContext(UserAuth);
+  const { serviceName, text, _id } = rev;
   const [show, setShow] = useState(false);
-
-
-  
-  
- 
+  const [update, setUpdate] = useState(rev)
 
   const deteleButton = () => {
-    const okay = window.confirm('Are you want to delete it?')
+    const okay = window.confirm("Are you want to delete it?");
     console.log(`http://localhost:5000/reviews/${_id}`);
     if (okay) {
       fetch(`http://localhost:5000/reviews/${_id}`, {
-        method: "DELETE"
+        method: "DELETE",
       })
-        .then(res => res.json)
-      .then(data=>{
-        toast.success("You review has been removed");
-        console.log(data)
-      })
+        .then((res) => res.json)
+        .then((data) => {
+          toast.success("You review has been removed");
+          console.log(data);
+        });
     }
-  }
+  };
 
   const handleShow = () => setShow(true);
   const handleClose = (e) => {
-    console.log(e.target.innerText);
-    console.log(e.target.parentNode.parentNode.children[1].innerHTML);
+    const action = e.target.value
+    const text = document.getElementById("text-input").value;
     setShow(false);
+    if (action === 'update') {
+      fetch(`http://localhost:5000/reviews/${_id}`, {
+        method: 'PUT',
+        headers: {
+          'content-type':'application/json'
+        },
+        body:JSON.stringify(text)
+      })
+        .then(res=>res.json())
+        .then(data => {
+          console.log(data);
+          toast.success('Review update')
+        })
+      .catch()
+    }
+    
+    console.log(text);
   };
-    return (
-      <div>
-        {/* review card */}
-        <Card className="mb-4">
-          <Card.Header>
-            <h3>ServiceName: {serviceName}</h3>
-          </Card.Header>
-          <Card.Body>
-            <Card.Title>{user?.displayName}</Card.Title>
-            <Card.Text>
-              <FaQuoteLeft />
-              <p className="mb-0 my-2 fw-semibold">{text} </p>
-              <FaQuoteRight className="mb-0" />
-            </Card.Text>
-            <div className="text-end">
-              <Button
-                variant="outline-danger"
-                className="me-2"
-                onClick={deteleButton}
-              >
-                Delete
-              </Button>
-              <Button variant="outline-success" onClick={handleShow}>
-                Update
-              </Button>
-            </div>
-          </Card.Body>
-        </Card>
-        {/* _____ */}
+  return (
+    <div>
+      {/* review card */}
+      <Card className="mb-4">
+        <Card.Header>
+          <h3>ServiceName: {serviceName}</h3>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>{user?.displayName}</Card.Title>
+          <Card.Text>
+            <FaQuoteLeft />
+            <p className="mb-0 my-2 fw-semibold">{text} </p>
+            <FaQuoteRight className="mb-0" />
+          </Card.Text>
+          <div className="text-end">
+            <Button
+              variant="outline-danger"
+              className="me-2"
+              onClick={deteleButton}
+            >
+              Delete
+            </Button>
+            <Button variant="outline-success" onClick={handleShow}>
+              Update
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+      {/* _____ */}
 
-        {/* modal section  */}
-        <Form>
-          <Modal
-            show={show}
-            onHide={handleClose}
-            animation={false}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-          >
-            <Modal.Header closeButton>
-              <Modal.Title>Update your review</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form.Control as="textarea" rows={3} />
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Cancel
-              </Button>
-              <Button variant="primary" onClick={handleClose}>
-                Update
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </Form>
-      </div>
-    );
+      {/* modal section  */}
+      <Form>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          animation={false}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Update your review</Modal.Title>
+          </Modal.Header>
+          <Modal.Body name="name">
+            <textarea class="form-control" id="text-input" rows="3" defaultValue={text}></textarea>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Update
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Form>
+    </div>
+  );
 };
 
 export default RevCard;
